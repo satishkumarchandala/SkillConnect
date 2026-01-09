c6cc0ab06b679be3fd89ae005bf5bded7f16ec6a
Yunusr714 <yunusabdul274@gmail.com>
2026-01-09 10:24:57 +0530

    added login page

--- SkillConnect/src/app/app.routes.ts
+++ SkillConnect/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: 'login',
+		loadComponent: () =>
+			import('./login/login.component').then((m) => m.LoginComponent),
+	},
+	{
+		path: '',
+		pathMatch: 'full',
+		redirectTo: 'login',
+	},
+	{
+		path: '**',
+		redirectTo: 'login',
+	},
+];
---
+++ SkillConnect/src/app/login/login.component.ts
@@ -0,0 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  readonly form: FormGroup;
+
+  constructor(private readonly fb: FormBuilder) {
+    this.form = this.fb.group({
+      mobile: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const { mobile, password } = this.form.value;
+    // TODO: Replace with real auth call
+    console.log('Login attempt', { mobile, password });
+  }
+
+  get mobileCtrl() {
+    return this.form.get('mobile');
+  }
+
+  get passwordCtrl() {
+    return this.form.get('password');
+  }
+}
