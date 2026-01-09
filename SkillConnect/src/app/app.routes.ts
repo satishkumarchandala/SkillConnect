a55ebdadc35ac84750496f17156fb47e7f4916ab
Yunusr714 <yunusabdul274@gmail.com>
2026-01-09 10:34:15 +0530

    updated gliches

--- SkillConnect/src/app/app.routes.ts
+++ SkillConnect/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { Register } from './register/register';
-import { Login } from './login/login';
 import { Home } from './home/home';
 
 export const routes: Routes = [
@@ -8,17 +7,8 @@ export const routes: Routes = [
     path: 'login',
     loadComponent: () => import('./login/login.component').then((m) => m.LoginComponent),
   },
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'login',
-  },
-  {
-    path: '**',
-    redirectTo: 'login',
-  },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: Login },
+
+ 
   { path: 'register', component: Register },
   { path: 'home', component: Home }
 ];
